a94fe328fb9d458a07ea457ddfec1405096e3ebd
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-05-17 14:48:43 -0300

    feat(mobile): add pre login screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,14 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { styled } from 'nativewind'
+
 import { useFonts } from './src/hooks'
+
+import NLWLogo from './src/common/assets/nlw-spacetime-logo.svg'
+import blurBg from './src/common/assets/bg-blur.png'
+
+import StripesComponent from './src/common/assets/stripes.svg'
+const Stripes = styled(StripesComponent)
 
 export default function App() {
   const { hasLoadedFonts, errors } = useFonts()
@@ -14,9 +22,44 @@ export default function App() {
   }
 
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="font-alt text-5xl text-zinc-50">Rocketseat</Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center justify-center bg-gray-900 px-8 py-10"
+      imageStyle={{
+        position: 'absolute',
+        left: '-100%',
+      }}
+    >
+      <Stripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.8}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            COMEÇAR A CADASTRAR
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }

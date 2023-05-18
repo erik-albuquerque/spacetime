c4675d258bb44b06f68538ebab58dbf9a0e2beb4
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-05-17 23:13:52 -0300

    feat(server): add plugin cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,10 @@
+import cors from '@fastify/cors'
 import { app, initServer } from './constants/fastify'
 import { memoriesRoutes } from './routes'
 
 const bootstrap = async () => {
+  app.register(cors, { origin: true })
+
   await app.register(memoriesRoutes)
 
   await initServer()

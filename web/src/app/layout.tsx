1ff36486454d8ec68cc0419a4f6fc219996c5452
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-05-19 13:11:16 -0300

    feat(web): add logout route

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -17,32 +17,6 @@ export const metadata = {
     'Uma cápsula do tempo construída com React, Next.js, TailwindCSS e TypeScript',
 }
 
-const Profile = () => {
-  const { getUser } = useUser()
-
-  const { name, avatarUrl } = getUser()
-
-  return (
-    <div className="flex items-center gap-3 text-left">
-      {/* Section Avatar */}
-      <Image
-        src={avatarUrl}
-        width={40}
-        height={40}
-        alt=""
-        className="h-10 w-10 rounded-full"
-      />
-
-      <p className="max-w-[140px] text-sm leading-snug">
-        {name}
-        <a href="" className="block text-red-400 hover:text-red-300">
-          Quero sair
-        </a>
-      </p>
-    </div>
-  )
-}
-
 export default function RootLayout({ children }: { children: ReactNode }) {
   const { isAuthenticated } = useUser()
 
@@ -133,5 +107,34 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         </main>
       </body>
     </html>
+  )
+}
+
+const Profile = () => {
+  const { getUser } = useUser()
+
+  const { name, avatarUrl } = getUser()
+
+  return (
+    <div className="flex items-center gap-3 text-left">
+      {/* Section Avatar */}
+      <Image
+        src={avatarUrl}
+        width={40}
+        height={40}
+        alt=""
+        className="h-10 w-10 rounded-full"
+      />
+
+      <p className="max-w-[140px] text-sm leading-snug">
+        {name}
+        <a
+          href="/api/auth/logout"
+          className="block text-red-400 hover:text-red-300"
+        >
+          Quero sair
+        </a>
+      </p>
+    </div>
   )
 }
---
+++ web/src/app/memories/new/page.tsx
@@ -0,0 +1,5 @@
+const NewMemories = () => {
+  return <span>new</span>
+}
+
+export default NewMemories

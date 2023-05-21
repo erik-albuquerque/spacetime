f0a5c266f821cc661a63a40c359cdc87993a46cf
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-05-21 20:53:49 -0300

    feat(web): add list memories

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,13 +1,86 @@
-const Home = () => {
+import Image from 'next/image'
+import Link from 'next/link'
+import dayjs from 'dayjs'
+import ptBR from 'dayjs/locale/pt-br'
+
+import { useAuth } from '@/hooks'
+import { api } from '@/lib'
+import { ArrowRight } from 'lucide-react'
+
+dayjs.locale(ptBR)
+
+type Memory = {
+  id: string
+  coverUrl: string
+  excerpt: string
+  createdAt: string
+}
+
+const Home = async () => {
+  const { isAuthenticated, token } = useAuth()
+
+  if (!isAuthenticated) {
+    return (
+      <div className="flex flex-1 items-center justify-center p-16">
+        <p className="w-[360px] text-center leading-relaxed">
+          Você ainda não registrou nenhuma lembrança, comece a{' '}
+          <a href="" className="underline transition-colors hover:text-gray-50">
+            criar agora
+          </a>
+          !
+        </p>
+      </div>
+    )
+  }
+
+  const response = await api.get('/memories', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const memories: Memory[] = response.data.memories
+
+  if (memories.length === 0) {
+    return (
+      <div className="flex flex-1 items-center justify-center p-16">
+        <p className="w-[360px] text-center leading-relaxed">
+          Você ainda não registrou nenhuma lembrança, comece a{' '}
+          <a href="" className="underline transition-colors hover:text-gray-50">
+            criar agora
+          </a>
+          !
+        </p>
+      </div>
+    )
+  }
+
   return (
-    <div className="flex flex-1 items-center justify-center">
-      <p className="w-[360px] text-center leading-relaxed">
-        Você ainda não registrou nenhuma lembrança, comece a{' '}
-        <a href="" className="underline transition-colors hover:text-gray-50">
-          criar agora
-        </a>
-        !
-      </p>
+    <div className="flex flex-col gap-10 p-8">
+      {memories.map((memory) => (
+        <div key={memory.id} className="space-y-4">
+          <time className="flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
+            {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
+          </time>
+          <Image
+            src={memory.coverUrl}
+            alt=""
+            width={592}
+            height={280}
+            className="aspect-video w-full rounded-lg object-cover"
+          />
+          <p className="text-lg leading-relaxed text-gray-100">
+            {memory.excerpt}
+          </p>
+          <Link
+            href={`/memories/${memory.id}`}
+            className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
+          >
+            Ler mais
+            <ArrowRight className="h-4 w-4" />
+          </Link>
+        </div>
+      ))}
     </div>
   )
 }

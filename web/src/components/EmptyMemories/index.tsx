1c9e751f2268913ea7ec2d9bbc3ff3f75bb6089f
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-05-22 20:22:10 -0300

    feat(web): add EmptyMemories component

---
+++ web/src/components/EmptyMemories/index.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+const EmptyMemories = () => {
+  return (
+    <div className="flex flex-1 items-center justify-center p-16">
+      <p className="w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, comece a{' '}
+        <Link
+          href="/memories/new"
+          className="underline transition-colors hover:text-gray-50"
+        >
+          criar agora
+        </Link>
+        !
+      </p>
+    </div>
+  )
+}
+
+export { EmptyMemories }
--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -1,1 +1,2 @@
 export * from './MediaPicker'
+export * from './EmptyMemories'
